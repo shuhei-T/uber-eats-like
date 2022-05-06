d82d849d4b83b6081ffbd6c86f121afdd623da30
shuhei-T <nofuture742617000027@gmail.com>
2022-05-06 15:37:37 +0900

    [Add] React開発環境を構築 #4

---
+++ frontend/src/style_constants.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+  MAIN: '#02A457',
+  BOADER: '#e2e2e2',
+  SUB_TEXT: '#545454',
+  SUB_BUTTON: '#eeeeee',
+}
+
+export const FONT_SIZE = {
+  STAND_BODY: '24px',
+  BODY1: '16PX',
+  BODY2: '14PX',
+}
